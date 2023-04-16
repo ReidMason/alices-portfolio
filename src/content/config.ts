79dc268b3f54da956ba1d45413c814134c907fe8
Reid Mason <maddogshain132@gmail.com>
2023-04-16 19:03:10 +0100

    Initial publish with projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,12 +4,15 @@ import { z } from "zod";
 export const collections = {
   project: defineCollection({
     schema: z.object({
+      index: z.number(),
       title: z.string(),
-      description: z.string(),
+      description: z.string().optional(),
       mainImage: z.string(),
       images: z.array(z.object({
+        index: z.number(),
         url: z.string(),
         alt: z.string(),
+        disabled: z.boolean().optional()
       })),
     }),
   }),
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+import { getCollection } from "astro:content";
+
+export const getProjects = async () => {
+  const projects = await getCollection("project");
+  projects.sort((a, b) => a.data.index - b.data.index);
+  return projects;
+}

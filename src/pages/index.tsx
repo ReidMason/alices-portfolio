8f65335e1ac6805cf6c46a748db561136b89477c
Reid Mason <maddogshain132@gmail.com>
2023-04-19 22:06:51 +0100

    Started adding images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image, { type StaticImageData } from "next/image";
 
 interface Project {
   name: string,
-  image: string,
+  mainImage: StaticImageData,
 }
 
 interface HomeProps {
@@ -16,15 +17,26 @@ const Home: NextPage<HomeProps> = ({ projects }: HomeProps) => {
   return (
     <>
       <Head>
-        <title>Alice's portfolio</title>
+        <title>Alice&apos;s portfolio</title>
         <meta name="description" content="Alice's portfolio" />
       </Head>
-      <main>
-        {
-          projects.map(x => (
-            <div>{x.name}</div>
-          ))
-        }
+      <main className="flex flex-col w-full items-center p-20">
+        <ul className="grid md:grid-cols-2 gap-24 w-full">
+          {
+            projects.map(x => (
+              <li key={x.name}>
+                <a className="group" href={`/project/${x.name}`} >
+                  <div className="relative pb-[75%] w-full overflow-hidden">
+                    {/* <div className="overflow-hidden absolute h-full group-hover:opacity-90 transition-opacity duration-200"> */}
+                    <Image className="absolute position-center" src={x.mainImage} alt="idk" />
+                    {/* </div> */}
+                  </div>
+                  <p className="mt-3 text-xl">{x.name}</p>
+                </a>
+              </li>
+            ))
+          }
+        </ul>
       </main>
     </>
   );
@@ -32,12 +44,19 @@ const Home: NextPage<HomeProps> = ({ projects }: HomeProps) => {
 
 export default Home;
 
+import tiotMainImage from "../assets/images/the-importance-of-touch/main-image.jpeg";
 // This function gets called at build time
-export async function getStaticProps() {
+export function getStaticProps() {
+
   const projects: Project[] = [{
     name: "The importance of touch",
-    image: "idk"
-  }];
+    mainImage: tiotMainImage,
+  },
+  {
+    name: "The importance of touch",
+    mainImage: tiotMainImage,
+  }
+  ];
 
   return {
     props: {

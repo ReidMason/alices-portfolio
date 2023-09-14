8dd3a6011d9cb5c47ba105d48740d5c97afdd1f6
Reid Mason <maddogshain132@gmail.com>
2023-09-14 20:54:16 +0100

    Added typesafety to satisfy typescript

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,17 @@
 import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
 
-export const getProjects = async () => {
+export type ProjectCollectionEntry = CollectionEntry<"project">;
+
+export const getProjects = async (): Promise<ProjectCollectionEntry[]> => {
   const projects = await getCollection("project");
   projects.sort((a, b) => a.data.index - b.data.index);
   projects.map((x) => x.data.images.sort((a, b) => a.index - b.index));
   return projects;
+};
+
+export const getMainImage = (project: ProjectCollectionEntry) => {
+  return (
+    project.data.images.find((x) => x.isMainImage) ?? project.data.images[0]
+  );
 };

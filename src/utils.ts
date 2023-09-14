df6bbc8f2812ba7fd84f19f1e22fe2904f5e9cef
Reid Mason <maddogshain132@gmail.com>
2023-09-14 20:25:40 +0100

    Added support for index sorting of images

--- src/utils.ts
+++ src/utils.ts
@@ -3,5 +3,6 @@ import { getCollection } from "astro:content";
 export const getProjects = async () => {
   const projects = await getCollection("project");
   projects.sort((a, b) => a.data.index - b.data.index);
+  projects.map((x) => x.data.images.sort((a, b) => a.index - b.index));
   return projects;
 };

f0b90efb4d31a3fc5d1c4e1dd7ae178273568e7f
Reid Mason <maddogshain132@gmail.com>
2024-01-22 14:43:14 +0000

    Switched to using a json file for content generation

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
 import { getCollection } from "astro:content";
 import type { CollectionEntry } from "astro:content";
+import content from "./content.json";
 
 export type ProjectCollectionEntry = CollectionEntry<"project">;
 
-export const getProjects = async (): Promise<ProjectCollectionEntry[]> => {
-  const projects = await getCollection("project");
-  projects.sort((a, b) => a.data.index - b.data.index);
-  projects.map((x) => x.data.images.sort((a, b) => a.index - b.index));
-  return projects;
+export type Project = typeof content[0];
+
+export const getProjects = () => {
+  return content;
 };
 
 export const getMainImage = (project: ProjectCollectionEntry) => {
